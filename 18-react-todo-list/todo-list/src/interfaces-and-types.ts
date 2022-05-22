e1552db088d4ca27c337725576d42a693896dc85
Ilya Bayanov <i.p.bayanov@gmail.com>
2022-05-22 22:48:19 +0300

    remove unnecessary interfaces

--- 18-react-todo-list/todo-list/src/interfaces-and-types.ts
+++ 18-react-todo-list/todo-list/src/interfaces-and-types.ts
@@ -3,20 +3,3 @@ export interface ITodoItem {
   title: string;
   completed: boolean;
 }
-
-export interface ITodoItemWithCBs extends ITodoItem {
-  edit: Function;
-  toggle: Function;
-  delete: Function;
-}
-
-export interface IMainProps {
-  todos: Array<ITodoItem>;
-  edit: Function;
-  toggle: Function;
-  toggleAll: Function;
-  delete: Function;
-  filter: number;
-  filterTodos: Function;
-  clearComplited: Function;
-}
